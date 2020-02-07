77cdd401776108c87502ccdebcbb6797a90c0eb8
Seiya <e1517234@soka-u.jp>
2020-02-07 02:11:54 +0000

    add: subnetの設定をコード化するためのファイルを追加しました

--- prod/network.tf
+++ prod/network.tf
@@ -1,4 +1,11 @@
 resource "aws_vpc" "myvpc" {
- cidr_block = "${var.cidr_block}"
+ cidr_block = "${var.vpc_cidr_block}"
  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "mysubnet" {
+  availability_zone = "${var.availability_zone}"
+  cidr_block        = "${var.subnet_cidr_block}"
+  vpc_id            = "${var.vpc_id}"
+  map_public_ip_on_launch = true
 }
--- prod/variables.tf
+++ prod/variables.tf
@@ -1,4 +1,7 @@
 variable "aws_access_key_id" {}
 variable "aws_secret_access_key" {}
 variable "region" {}
-variable "cidr_block" {}
+variable "vpc_cidr_block" {}
+variable "vpc_id" {}
+variable "subnet_cidr_block" {}
+variable "availability_zone" {}
